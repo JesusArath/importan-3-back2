0d2fb86afe851a2dc254a85ab2978563d01250b5
Jesus F <jesus19arath@hotmail.com>
2020-12-06 23:00:22 -0600

    make it better

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
+var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
+    return new(P || (P = Promise))(function(resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+
+        function step(result) { result.done ? resolve(result.value) : new P(function(resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
+var __importDefault = (this && this.__importDefault) || function(mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -33,8 +35,9 @@ app.post('/', (req, res) => {
     const resultado = `${datos}.00 pesos`;
     const fs = require('fs');
     const fetch = require("node-fetch");
+
     function modifyPdf() {
-        return __awaiter(this, void 0, void 0, function* () {
+        return __awaiter(this, void 0, void 0, function*() {
             const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf';
             // const existingPdfBytes = await fetch(url).then((res: any) => res.arrayBuffer())
             const existingPdfBytes = fs.readFileSync('./importan-plantilla.pdf');
